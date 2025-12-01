41b97817aba835e5a885c8a54f9ae507d44305ca
mcakif58 <mcakif58@gmail.com>
2025-12-01 18:42:35 +0300

    Projenin ilk versiyonu

---
+++ tailwinf.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0', transform: 'translateY(10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        'fade-in-fast': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        }
+      },
+      animation: {
+        'fade-in': 'fade-in 0.3s ease-out forwards',
+        'fade-in-fast': 'fade-in-fast 0.2s ease-out forwards',
+      }
+    },
+  },
+  plugins: [],
+}
